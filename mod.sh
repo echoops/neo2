7e873792bb38771cbdb7aee7f031486e37d3f118
白夜 <75304431+echoops@users.noreply.github.com>
2021-01-18 00:02:31 +0800

    After Update feeds
    Move upnp from services to network

--- mod.sh
+++ mod.sh
@@ -35,7 +35,7 @@ svn co https://github.com/echoops/Packages/trunk/luci-app-turboacc package/commu
 #sed -i "s/# REVISION:=x/REVISION:=Mod by echoops/g" include/version.mk
 #
 # > 修改插件位置
-sed -i 's/\"services\"/\"nas\"/g' feeds/luci/applications/luci-app-upnp/luasrc/controller/upnp.lua
+sed -i 's/\"services\"/\"network\"/g' feeds/luci/applications/luci-app-upnp/luasrc/controller/upnp.lua
 #
 # > 修改插件名字
 sed -i 's/"网络存储"/"存储"/g' package/lean/luci-app-amule/po/zh-cn/amule.po
@@ -45,8 +45,8 @@ sed -i 's/"Web 管理"/"Web"/g' package/lean/luci-app-webadmin/po/zh-cn/webadmin
 sed -i 's/"带宽监控"/"监视"/g' feeds/luci/applications/luci-app-nlbwmon/po/zh-cn/nlbwmon.po
 sed -i 's/"实时流量监测"/"流量"/g' package/lean/luci-app-wrtbwmon/po/zh-cn/wrtbwmon.po
 sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' package/community/luci-app-turboacc/po/zh-cn/turboacc.po
-sed -i 's/"KMS 服务器"/"系统激活服务器"/g' package/lean/luci-app-vlmcsd/po/zh-cn/vlmcsd.zh-cn.po
-sed -i 's/"TTYD 终端"/"命令窗"/g' package/lean/luci-app-ttyd/po/zh-cn/terminal.po
+sed -i 's/"KMS 服务器"/"服务激活"/g' package/lean/luci-app-vlmcsd/po/zh-cn/vlmcsd.zh-cn.po
+sed -i 's/"TTYD 终端"/"命令行"/g' package/lean/luci-app-ttyd/po/zh-cn/terminal.po
 sed -i 's/"USB 打印服务器"/"打印服务"/g' package/lean/luci-app-usb-printer/po/zh-cn/usb-printer.po
 sed -i 's/"解锁网易云灰色歌曲"/"网易云音乐解锁"/g' package/lean/luci-app-unblockmusic/po/zh-cn/unblockmusic.po
 #
