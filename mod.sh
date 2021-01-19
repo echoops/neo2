e173e6e81437425d51291c9268a89849433c4c45
白夜 <75304431+echoops@users.noreply.github.com>
2021-01-20 01:18:59 +0800

    After Update feeds

--- mod.sh
+++ mod.sh
@@ -51,6 +51,5 @@ sed -i 's/\"KMS 服务器\"/\"KMS 激活服务\"/g' package/lean/luci-app-vlmcsd
 sed -i 's/\"网络共享\"/\"SMB 文件共享\"/g' package/lean/luci-app-samba4/po/zh-cn/samba4.po
 sed -i 's/\"NFS 管理\"/\"NFS 文件共享\"/g' package/lean/luci-app-nfs/po/zh-cn/nfs.po
 sed -i 's/\"IP\/MAC绑定\"/\"静态 ARP\"/g' package/lean/luci-app-arpbind/po/zh-cn/arpbind.po
-sed -i 's/\"UPnP\"/\"端口转发\"/g' feeds/luci/applications/luci-app-upnp/po/zh-cn/upnp.po
 sed -i 's/\"Turbo ACC 网络加速\"/\"网络加速\"/g' package/lean/luci-app-flowoffload/po/zh-cn/flowoffload.po
 #
