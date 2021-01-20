26a7b5586bd01415ec4a520df34646f5488c2c0c
白夜 <75304431+echoops@users.noreply.github.com>
2021-01-20 18:35:04 +0800

    After update feeds

--- mod.sh
+++ mod.sh
@@ -7,6 +7,7 @@
 # > 清除旧版argon主题并拉取最新版
 rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/community/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config package/community/luci-app-argon-config
 #
 # > 修改默认主题为 Argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
@@ -39,18 +40,20 @@ sed -i '$i '"echo 'luciversion = \"Mod\"' >> /usr/lib/lua/luci/version.lua"'' pa
 #
 # > 修改插件位置
 sed -i 's/\"services\"/\"network\"/g' feeds/luci/applications/luci-app-upnp/luasrc/controller/upnp.lua
+sed -i '\"NAS\"/d' package/lean/luci-app-softethervpn/luasrc/controller/softethervpn.lua
 sed -i 's/\"nas\"/\"services\"/g' package/lean/luci-app-softethervpn/luasrc/controller/softethervpn.lua
-sed -i 's/\"nas\"/\"services\"/g' package/lean/luci-app-nfs/luasrc/controller/nfs.lua
 #
 # > 修改插件名字
+sed -i 's/\"Argon 主题设置\"/\"主题设置\"/g' package/community/luci-app-argon-config/po/zh-cn/argon-config.po
 sed -i 's/\"TTYD 终端\"/\"网页终端\"/g' package/lean/luci-app-ttyd/po/zh-cn/terminal.po
 sed -i 's/\"解锁网易云灰色歌曲\"/\"网易云音乐解锁\"/g' package/lean/luci-app-unblockmusic/po/zh-cn/unblockmusic.po
 sed -i 's/\"网易UU网游加速器\"/\"网易游戏加速器\"/g' package/lean/luci-app-uugamebooster/po/zh-cn/uugamebooster.po
 sed -i 's/\"SoftEther VPN 服务器\"/\"SoftEther VPN\"/g' package/lean/luci-app-softethervpn/po/zh-cn/softethervpn.po
-sed -i 's/\"动态 DNS\"/\"动态域名解析\"/g' feeds/luci/applications/luci-app-ddns/po/zh-cn/ddns.po
+sed -i 's/\"动态 DNS\"/\"DNS 动态域名\"/g' feeds/luci/applications/luci-app-ddns/po/zh-cn/ddns.po
+sed -i 's/\"Frp 内网穿透\"/\"FRP 内网穿透\"/g' package/lean/luci-app-frpc/po/zh-cn/frp.po
 sed -i 's/\"KMS 服务器\"/\"KMS 激活服务\"/g' package/lean/luci-app-vlmcsd/po/zh-cn/vlmcsd.zh-cn.po
-sed -i 's/\"网络共享\"/\"SMB 文件共享\"/g' package/lean/luci-app-samba4/po/zh-cn/samba4.po
-sed -i 's/\"NFS 管理\"/\"NFS 文件共享\"/g' package/lean/luci-app-nfs/po/zh-cn/nfs.po
+sed -i 's/\"网络共享\"/\"SMB 文件共享\"/g' feeds/luci/applications/luci-app-samba/po/zh-cn/samba.po
+sed -i 's/\"硬盘休眠\"/\"硬盘休眠助手\"/g' feeds/luci/applications/luci-app-hd-idle/po/zh-cn/hd_idle.po
 sed -i 's/\"IP\/MAC绑定\"/\"静态 ARP\"/g' package/lean/luci-app-arpbind/po/zh-cn/arpbind.po
 sed -i 's/\"Turbo ACC 网络加速\"/\"网络加速\"/g' package/lean/luci-app-flowoffload/po/zh-cn/flowoffload.po
 sed -i 's/\"带宽监控\"/\"监视\"/g' feeds/luci/applications/luci-app-nlbwmon/po/zh-cn/nlbwmon.po
