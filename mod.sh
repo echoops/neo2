db9d577793564ca9bb04c8f48aee7ec0f5aa6c00
白夜 <75304431+echoops@users.noreply.github.com>
2021-01-21 09:48:49 +0800

    After update feeds

--- mod.sh
+++ mod.sh
@@ -1,3 +1,43 @@
+#!/bin/bash
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+
+# 配置自定义主题
+#
+# > 清除旧版argon主题并拉取最新版
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/community/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config package/community/luci-app-argon-config
+#
+# > 修改默认主题为 Argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+#
+
+# 集成自定义插件
+#
+# > AdguardHome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/community/luci-app-adguardhome
+#
+# > Dockerman
+git clone https://github.com/lisaac/luci-lib-docker package/community/luci-lib-docker
+git clone https://github.com/lisaac/luci-app-dockerman package/community/luci-app-dockerman
+#
+# > HelloWorld (vssr)
+git clone https://github.com/jerrykuku/lua-maxminddb package/community/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr package/community/luci-app-vssr
+#
+# > TurboACC
+#svn co https://github.com/echoops/Packages/trunk/luci-app-turboacc package/community/luci-app-turboacc
+#
+
+# 个性化配置修改
+#
+# > Mod
+#sed -i '$i '"sed -i '/luciname/d' /usr/lib/lua/luci/version.lua"'' package/lean/default-settings/files/zzz-default-settings
+#sed -i '$i '"echo 'luciname = \"Limitless\"' >> /usr/lib/lua/luci/version.lua"'' package/lean/default-settings/files/zzz-default-settings
+sed -i '$i '"sed -i '/luciversion/d' /usr/lib/lua/luci/version.lua"'' package/lean/default-settings/files/zzz-default-settings
+sed -i '$i '"echo 'luciversion = \"Mod\"' >> /usr/lib/lua/luci/version.lua"'' package/lean/default-settings/files/zzz-default-settings
+#
 # > 修改插件位置
 sed -i 's/\"services\"/\"network\"/g' feeds/luci/applications/luci-app-upnp/luasrc/controller/upnp.lua
 sed -i '/\"VPN\"/d' package/lean/luci-app-softethervpn/luasrc/controller/softethervpn.lua
